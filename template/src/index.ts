ea7bb60de8c210b839228cb29d416516d90e51e0
Pricklety <karlonoob878@gmail.com>
2025-08-13 11:28:51 +0200

    Update index.ts

--- template/src/index.ts
+++ template/src/index.ts
@@ -1,10 +1,28 @@
 import { R2Explorer } from "r2-explorer";
 
 // Read all available configurations here: https://r2explorer.com/getting-started/configuration/
-export default R2Explorer({
-  readonly: false,
-  basicAuth: {
-    username: "pricklety",
-    password: process.env.PASS
+export default {
+  async fetch(request, env, ctx) {
+    return R2Explorer({
+      readonly: false,
+      cors: true,
+      showHiddenFiles: true,
+      basicAuth: {
+        username: env.BASIC_USERNAME,
+        password: env.BASIC_PASSWORD,
+      },
+    }).fetch(request, env, ctx);
+  },
+  async email(event, env, ctx) {
+    await R2Explorer({
+      readonly: false,
+      cors: true,
+      showHiddenFiles: true,
+      basicAuth: {
+        username: env.BASIC_USERNAME,
+        password: env.BASIC_PASSWORD,
+      },
+    }).email(event, env, ctx);
   }
-});
+};
+
