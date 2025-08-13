dff3c1b7fcf4a42679332a3af6086b8c6b97c7d8
Pricklety <karlonoob878@gmail.com>
2025-08-13 10:52:06 +0200

    Update index.ts

--- template/src/index.ts
+++ template/src/index.ts
@@ -3,8 +3,8 @@ import { R2Explorer } from "r2-explorer";
 // Read all available configurations here: https://r2explorer.com/getting-started/configuration/
 export default R2Explorer({
 	readonly: true,
-	// basicAuth: {
-	//   username: 'username',
-	//   password: 'password'
-	// }
+	basicAuth: {
+	  username: 'pricklety',
+	  password: 'JaSamKarlo12345!!'
+	}
 });
